67343a658345eec491de4754da215b7655961cb4
Faisal9776 <55557992+Faisal9776@users.noreply.github.com>
2020-09-16 05:47:07 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+				<div id="jquery-script-menu">
+					<div class="jquery-script-center">
+
+						<div class="jquery-script-ads">
+							<script type="text/javascript">
+								
+				</script>
+							<script type="text/javascript"
+								src="https://pagead2.googlesyndication.com/pagead/show_ads.js">
+							</script>
+						</div>
+						<div class="jquery-script-clear"></div>
+					</div>
+				</div>
+				<h1 style={{marginTop: "150px" }}></h1>
+				<div id="move-stats" style={{ position: 'absolute' ,left: "2%",top:"250px",padding:"10px",boxShadow:(0, 0, "8px", "2px", 'rgba(100, 100, 100, .4)'),fontFamily:'tahoma'}}></div>
+				<div id="contain" style={{position:'absolute',width:"50%",height:"60%",left:"40%",top:"250px",boxShadow:(0, 0, "8px", "2px", 'rgba(100, 100, 100, .4)')}}></div>
+
+				<button class="resize-done">Done</button>
+            </div>
+        );
+    }
+    }
+ 
+
+export default App;
